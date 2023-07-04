db6ffd7cd976b802b015e4b7caffeefe8bc55253
loarden <biacsi.ors@gmail.com>
2023-07-04 18:55:12 +0200

    emailjs

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import InViewAnimation from "./InViewAnimation";
 import { motion } from "framer-motion";
 import { Input, Textarea } from "@material-tailwind/react";
-import Button from "./Button";
+import emailjs from 'emailjs-com'
 
 const inputStyle = {
   inputStyle:
@@ -12,7 +12,11 @@ const inputStyle = {
   divStyle: "w-full",
 };
 function ContactForm() {
+
+  const formRef = useRef()
+
   const [email, setEmail] = useState({
+    name: "",
     email: "",
     subject: "",
     body: "",
@@ -22,8 +26,30 @@ function ContactForm() {
     setEmail((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('service_tmzno6i', 'template_pdpe4xo', formRef.current, 'bgKCC9p0J_Ht-PZQo')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  }
+
   return (
-    <form className="flex flex-col items-end sm:items-center gap-4 w-full">
+    <form onSubmit={sendEmail} ref={formRef} className="flex flex-col items-end sm:items-center gap-4 w-full">
+      <InViewAnimation className={inputStyle.divStyle}>
+        <Input
+          onChange={changeHandler}
+          name="name"
+          className={inputStyle.inputStyle}
+          required
+          variant="standard"
+          label="Your Name"
+        />
+      </InViewAnimation>
       <InViewAnimation className={inputStyle.divStyle}>
         <Input
           onChange={changeHandler}
@@ -55,17 +81,12 @@ function ContactForm() {
         />
       </InViewAnimation>
       <InViewAnimation>
-        <a
-          href={`mailto:biacsi.ors@gmail.com?subject=${email.subject}&body=${email.body}`}
+        <motion.button
+          whileHover={{ y: -5 }}
+          className={inputStyle.buttonStyle}
         >
-          <motion.button
-            type="button"
-            whileHover={{ y: -5 }}
-            className={inputStyle.buttonStyle}
-          >
-            Send
-          </motion.button>
-        </a>
+          Send
+        </motion.button>
       </InViewAnimation>
     </form>
   );
